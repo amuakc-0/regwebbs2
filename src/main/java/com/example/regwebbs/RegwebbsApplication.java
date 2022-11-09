4905fecf32b85145a506bde0685be081e3b89fc8
amuakc-0 <amuakc-0@student.ltu.se>
2022-11-09 20:51:45 +0100

    kelek

---
+++ src/main/java/com/example/regwebbs/RegwebbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.regwebbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegwebbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegwebbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/regwebbs/RegwebbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regwebbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegwebbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
